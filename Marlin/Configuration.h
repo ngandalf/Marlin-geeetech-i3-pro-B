2db1a693d4384177b022fc4cd791e14c9142c73d
Alexandre WICQUART <alex@ndre.local>
2017-08-28 20:51:07 +0200

    Fix 3DTouch probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -371,10 +371,10 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   #ifdef AUTO_BED_LEVELING_GRID
 
     // set the rectangle in which to probe
-    #define LEFT_PROBE_BED_POSITION 30
-    #define RIGHT_PROBE_BED_POSITION 170 // 200
-    #define BACK_PROBE_BED_POSITION 152 // 147
-    #define FRONT_PROBE_BED_POSITION 20
+    #define LEFT_PROBE_BED_POSITION 20
+    #define RIGHT_PROBE_BED_POSITION 185 // 200
+    #define BACK_PROBE_BED_POSITION 165 // 147
+    #define FRONT_PROBE_BED_POSITION 10
 
      // set the number of grid points per dimension
      // I wouldn't see a reason to go above 3 (=9 probing points on the bed)
@@ -397,8 +397,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   // X and Y offsets must be integers
-  #define X_PROBE_OFFSET_FROM_EXTRUDER -25 //4
-  #define Y_PROBE_OFFSET_FROM_EXTRUDER -37 //-43
+  #define X_PROBE_OFFSET_FROM_EXTRUDER -13 //4
+  #define Y_PROBE_OFFSET_FROM_EXTRUDER -34 //-43
   #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.58 //-1.4
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
